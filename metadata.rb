ce8f0883cbe8c5d8aca569f7fd05479f0a9d1408
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-03-21 15:17:11 +0400

    Update description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 version '0.1.0'
-description ''
+description 'A safer replacement for Chef git resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 supports 'ubuntu'
