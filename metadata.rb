360f486ea754630b9be1798c23703921f2bffdcc
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-03-21 15:18:55 +0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'agit'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '0.1.0'
+version '0.1.1'
 description 'A safer replacement for Chef git resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
@@ -11,3 +11,7 @@ supports 'ubuntu'
 provides :agit
 
 gem 'git_clone_url'
+
+scm_url = 'https://github.com/aspyatkin/agit-cookbook'
+source_url scm_url if respond_to?(:source_url)
+issues_url "#{scm_url}/issues" if respond_to?(:issues_url)
